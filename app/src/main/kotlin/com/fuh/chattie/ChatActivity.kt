ff865c1d59f286a9e13acc600181aade7d50d7c3
Bilous Nikita <lordnikius@gmail.com>
2017-08-09 18:17:59 +0300

    login and chat started

---
+++ app/src/main/kotlin/com/fuh/chattie/ChatActivity.kt
@@ -0,0 +1,16 @@
+package com.fuh.chattie
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by lll on 09.08.2017.
+ */
+class ChatActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.chat_activity)
+    }
+}
